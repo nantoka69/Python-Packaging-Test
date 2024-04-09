0c1723721f30c6e83f1da551ce5ad16efe56bf60
Dr. Jörg Richter <joerg.richter@nantoka.de>
2024-04-10 01:18:22 +0200

    Added dependency to simple packaging test.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
     name='nantoka-python-packaging-complex',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages('source_root_1') + find_namespace_packages('source_root_2'),
     package_dir={
         '': 'source_root_1',
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         'matplotlib>=3.8.0',
         "numpy~=1.26.4",
+        "python-simple-packaging-test @ https://github.com/nantoka69/Python-Simple-Packaging-Test/releases/download/v1.0.0/python_simple_packaging_test-1.0.0-py3-none-any.whl"
     ],
     package_data={
         'package_1': ['data/*.json', 'data/*.html']
